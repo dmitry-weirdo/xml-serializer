470230b85f2768f7fb1f0147a185c847846c64db
dmitry.weirdo <dmitry.weirdo@gmail.com>
2014-10-31 02:57:42 +0000

    Подчищен класс Main. В run.bat внесены примеры вызова всех команд.

--- batExecute/run.bat
+++ batExecute/run.bat
@@ -1,2 +1,19 @@
-"%java_home%\bin\java" -cp ".\*;.\lib\*" ru.pda.xmlSerializer.Main INSERT_TEST_DATA 200 100
+rem
+rem ===========================================================================
+rem Load test data into the database
+"%java_home%\bin\java" -cp ".\*;.\lib\*" ru.pda.xmlSerializer.Main INSERT_TEST_DATA 4 1
+
+rem
+rem ===========================================================================
+rem Export data from database to xml file
 "%java_home%\bin\java" -cp ".\*;.\lib\*" ru.pda.xmlSerializer.Main EXPORT_DATA_TO_XML "departmentJobsFromDB.xml"
+
+rem
+rem ===========================================================================
+rem Export changed data from xml file to database
+"%java_home%\bin\java" -cp ".\*;.\lib\*" ru.pda.xmlSerializer.Main IMPORT_DATA_FROM_XML "departmentJobsChanged.xml"
+
+rem
+rem ===========================================================================
+rem Try to export data with coinciding natural keys ("departmentCode" and "jobName") fields
+"%java_home%\bin\java" -cp ".\*;.\lib\*" ru.pda.xmlSerializer.Main IMPORT_DATA_FROM_XML "departmentJobsCoincidingNaturalKeys.xml"
--- src/main/java/ru/pda/xmlSerializer/Main.java
+++ src/main/java/ru/pda/xmlSerializer/Main.java
@@ -12,7 +12,6 @@ import ru.pda.xmlSerializer.commandExecutor.ExportDataCommandExecutor;
 import ru.pda.xmlSerializer.commandExecutor.ImportDataCommandExecutor;
 import ru.pda.xmlSerializer.commandExecutor.InsertTestDataCommandExecutor;
 import ru.pda.xmlSerializer.commandLine.*;
-import ru.pda.xmlSerializer.jdbc.JdbcConnector;
 import ru.pda.xmlSerializer.jdbc.JdbcDriverRegisterFailException;
 import ru.pda.xmlSerializer.loggerConfig.LoggerConfigurator;
 import ru.pda.xmlSerializer.loggerConfig.LoggerConfiguringException;
@@ -20,78 +19,28 @@ import ru.pda.xmlSerializer.propertiesConfig.IncorrectPropertiesFileException;
 import ru.pda.xmlSerializer.propertiesConfig.PropertiesConfig;
 import ru.pda.xmlSerializer.xml.IncorrectXmlFileException;
 import ru.pda.xmlSerializer.xml.SerializeToXmlFileException;
-import ru.pda.xmlSerializer.xml.XmlSerializer;
-import su.opencode.kefir.util.ObjectUtils;
 
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import static su.opencode.kefir.util.StringUtils.concat;
 
+/**
+ * Main-класс приложения, запускаемый из&nbsp;командной строки с&nbsp;параметрами.
+ */
 public class Main
 {
-	public static void main(String[] args) throws IncorrectXmlFileException, NonUniqueNaturalKeyException, LoggerConfiguringException, IncorrectPropertiesFileException, JdbcDriverRegisterFailException, SQLException, SerializeToXmlFileException {
-		PropertiesConfig config = parsePropertiesConfig(); // файл свойств нужен для конфигурации логгера
-		configureLogger(config); // логгер может быть нужен для валидации аргументов командной строки
-		CommandArguments arguments = parseArguments(args);
-		executeCommand(arguments, config);
-
-		if (true)
-			return;
-
-		logger.info("From null enum");
-
-
-//		BasicConfigurator.configure();
-
-		System.out.println("arguments: " + Arrays.toString(args));
-		// todo: validate arguments
-
-		// read data from properties file
-//		String propertiesFileName = "xmlSerializer.properties"; // todo: possibly get it from program arguments
-		String propertiesFileName = "C:\\java\\xmlSerializer-googleCode\\src\\main\\resources\\properties\\xmlSerializer.properties"; // todo: possibly get it from program arguments
-//		PropertiesConfig config = ObjectFiller.createObject(propertiesFileName, PropertiesConfig.class);
-		System.out.println("PropertiesConfig:\n" + ObjectUtils.instanceToString(config));
-
-		// todo: validate fields
-
-		JdbcConnector connector = new JdbcConnector();
-		Connection connection = connector.createConnection(config);
-
-//		List<DepartmentJob> jobsToInsert = getJobs();
-		List<DepartmentJob> jobsToInsert = DepartmentJobGenerator.generateJobs(1000, 100, true);
-
-/*
-		System.out.println("time before insert:" + new Date() );
-		connector.insertJobs(connection, jobsToInsert);
-		System.out.println("time after insert:" + new Date() );
-*/
-
-		String xmlFileName = "c:\\java\\xmlSerializer\\jobs.xml";
-/*
-		DepartmentJobs jobsToSerialize = new DepartmentJobs();
-		jobsToSerialize.setJobs(jobsToInsert);
-		System.out.println( "serialized jobs size: " + jobsToSerialize.getJobs().size() );
-*/
-
-		XmlSerializer serializer = new XmlSerializer();
-/*
-		serializer.serializeToXml(jobsToSerialize, xmlFileName);
-
-		DepartmentJobs jobsFromXml = serializer.deserializeFromXml(xmlFileName);
-		System.out.println("deserialized jobs size (JAXB parser): " + jobsFromXml.getJobs().size());
-*/
-
-//		String xmlFileName1 = "c:\\java\\xmlSerializer\\jobs1.xml";
-//		String xmlFileName1 = "C:\\java\\xmlSerializer\\src\\test\\resources\\xml\\departmentJobInnerElements.xml";
-		DepartmentJobs jobsFromXmlDom = serializer.deserializeFromXmlUsingDomParser(xmlFileName);
-		System.out.println("deserialized jobs size (Xml DOM parser): " + jobsFromXmlDom.getJobs().size());
-
-		DepartmentJobMapUtils.createJobsMap(jobsFromXmlDom);
-		System.out.println("HashMap created successfully");
+	public static void main(String[] args) {
+		try
+		{
+			PropertiesConfig config = parsePropertiesConfig(); // файл свойств нужен для конфигурации логгера
+			configureLogger(config); // логгер может быть нужен для валидации аргументов командной строки
+			CommandArguments arguments = parseArguments(args);
+			executeCommand(arguments, config);
+		}
+		catch (Exception e)
+		{ // todo: здесь можно написать отдельное сообщение об ошибке для каждого типа исключения, но не в 6.30 утра. Так исключения будут в стектрейсе
+			UserMessageLogger.logError(logger, "Error while executing the program", e);
+		}
 	}
 
 	private static PropertiesConfig parsePropertiesConfig() throws IncorrectPropertiesFileException {
@@ -123,26 +72,16 @@ public class Main
 				break;
 
 			default:
-				throw new IllegalArgumentException(concat("Unknown command: \"", arguments.getCommand(), "\""));
+				throw new IllegalArgumentException( concat("Unknown command: \"", arguments.getCommand(), "\"") );
 		}
 	}
 
-	private static List<DepartmentJob> getJobs() {
-		List<DepartmentJob> jobs = new ArrayList<>();
-
-		final String departmentCode = "New department";
-
-		jobs.add( new DepartmentJob(departmentCode, "Работа раз") );
-		jobs.add( new DepartmentJob(departmentCode, "Работа два") );
-		jobs.add( new DepartmentJob(departmentCode, "Работа три") );
-		jobs.add( new DepartmentJob(departmentCode, "Работа четыре", "А вот эта работа будет с комментом") ); // with no comments
-
-		return jobs;
-	}
-
-	private StringBuffer sb = new StringBuffer();
 	private static final Logger logger = Logger.getLogger(Main.class);
 
+	/**
+	 * Имя файла настроек приложения.
+	 * // todo: можно принимать его в аргументах командной строки
+	 */
 	public static final String PROPERTIES_CONFIG_FILE_NAME = "xmlSerializer.properties";
 //	public static final String PROPERTIES_CONFIG_FILE_NAME = "C:\\java\\xmlSerializer-googleCode\\batExecute\\xmlSerializer.properties ";
 }
